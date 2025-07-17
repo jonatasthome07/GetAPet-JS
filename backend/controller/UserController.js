d1e36f88f9489dd76149754266de83dcd0f5536b
Jônatas <jonatasthome04@gmail.com>
2025-07-17 19:16:59 -0300

    Validations with status and json was added

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -2,6 +2,31 @@ import User from "../models/User.js"
 
 export default class UserController {
     static async register(req,res){
-        res.json({message: "Olá get a pet!"})
+        const {name, email, password, confirmpassword, phone} = req.body
+        
+        //Validations
+        if(!name){
+            return res.status(422).json({message: "Campo obrigatório!"})
+        }
+        if(!email){
+            return res.status(422).json({message: "Campo obrigatório!"})
+        }
+        if(!password){
+            return res.status(422).json({message: "Campo obrigatório!"})
+        }
+        if(!confirmpassword){
+            return res.status(422).json({message: "Campo obrigatório!"})
+        }
+        if(!phone){
+            return res.status(422).json({message: "Campo obrigatório!"})
+        }
+        if(password !== confirmpassword){
+            return res.status(422).json({message: "Senha e confirmação de senha devem ser iguais!"})
+        }
+
+        const checkUser = await User.findOne({email:email})
+        if (checkUser){
+            return res.status(422).json({message: "Usuário já existente!"})
+        }
     }
 }
