2609b368dc67d3bd59eec20a63b23a1fc42892d1
Jônatas <jonatasthome04@gmail.com>
2025-07-17 05:30:16 -0300

    First commit - Initial setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static("public"))
+
+//Permite o front se comunicar com o back, salvando sessões e enviando headers/tokens
+app.use(cors({credentials:true, origin:process.env.CLIENT_URL}))
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Conexão realizada com sucesso!")
+})
