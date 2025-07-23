152c60c70c716429a25061bdd19240db750e4d3b
Jônatas <jonatasthome04@gmail.com>
2025-07-23 15:46:31 -0300

    Started to apply images upload

---
+++ backend/helpers/imageUpload.js
@@ -0,0 +1,33 @@
+import multer from "multer"
+import path from "path"
+
+const imageStorage = multer.diskStorage({
+    //Define em qual pasta será salva, dependendo da URL acessada
+    destination: function (req,file,callback){
+        let folder = ""
+        if (req.baseUrl.includes("users")){
+            folder = "users"
+        } else if (req.baseUrl.includes("pets")){
+            folder = "pets"
+        }
+        callback(null, `public/images/${folder}`)
+    },
+    //Define o nome do arquivo
+    filename: function(req,file,callback){
+        callback(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const imageUpload = multer ({
+    //Onde o arquivo será salvo
+    storage: imageStorage,
+    //Filtra os tipos de arquivos que podem ser salvos
+    fileFilter(req,file,callback){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            return callback(new Error("Formato de arquivo inválido!"))
+        }
+        callback(undefined, true)
+    }
+})
+
+export default imageUpload
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,14 @@ import express from "express"
 import UserController from "../controller/UserController.js"
 const router = express.Router()
 
+//Middlewares
 import verifyToken from "../helpers/verifyToken.js"
+import imageUpload from "../helpers/imageUpload.js"
 
 router.post("/register", UserController.register)
 router.post("/login", UserController.login)
 router.get("/checkuser", UserController.checkUser)
 router.get("/:id", UserController.getUserById)
-router.patch("/edit/:id", verifyToken, UserController.editUser)
+router.patch("/edit/:id", verifyToken, imageUpload.single("image"),UserController.editUser)
 
 export default router
